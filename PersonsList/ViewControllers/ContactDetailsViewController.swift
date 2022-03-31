2768472ec6e26cf3c76c7bf2a46f3a8a1d0c051e
Aleksandra Moiseenko <lesovskaya.aleks@mail.ru>
2022-03-31 16:03:24 +0300

    All controllers were added to the project.

---
+++ PersonsList/ViewControllers/ContactDetailsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ContactDetailsViewController.swift
+//  PersonsList
+//
+//  Created by Александра Лесовская on 31.03.2022.
+//
+
+import UIKit
+
+class ContactDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ PersonsList/ViewControllers/ContactListTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ContactListTableViewController.swift
+//  PersonsList
+//
+//  Created by Александра Лесовская on 31.03.2022.
+//
+
+import UIKit
+
+class ContactListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+
+}
